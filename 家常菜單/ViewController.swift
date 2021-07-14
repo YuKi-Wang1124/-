46b61d0654a9ab982e0c8034488c0136bd7e1828
yuki <wkiki1124@gmail.com>
2021-07-15 01:11:37 +0800

    UIButton

--- 家常菜單/ViewController.swift
+++ 家常菜單/ViewController.swift
@@ -8,7 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var EggShrimp: UIButton!{
+        didSet {
+            EggShrimp.configurationUpdateHandler = { EggShrimp in
+                
+                EggShrimp.alpha = EggShrimp.isHighlighted ? 0.5 : 1
+            }
+        }
+    }
+    
+    @IBOutlet weak var RoastedMochi: UIButton! {
+        didSet {
+        RoastedMochi.configurationUpdateHandler = { [self] button in
+                
+                self.RoastedMochi.alpha = RoastedMochi.isHighlighted ? 0.5 : 1
+            }
+        }
+        }
+    @IBOutlet weak var BraisedPork: UIButton!{
+        didSet {
+            BraisedPork.configurationUpdateHandler = { BraisedPork in
+                
+                self.BraisedPork.alpha = BraisedPork.isHighlighted ? 0.5 : 1
+            }
+        }
+        }
+    @IBOutlet weak var WaterSpinachPork: UIButton!{
+        didSet {
+            WaterSpinachPork.configurationUpdateHandler = { WaterSpinachPork in
+                
+                self.WaterSpinachPork.alpha = WaterSpinachPork.isHighlighted ? 0.5 : 1
+            }
+        }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +49,6 @@ class ViewController: UIViewController {
 
 
 }
+    
+        
 
